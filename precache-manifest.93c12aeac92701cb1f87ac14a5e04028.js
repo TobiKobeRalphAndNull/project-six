3ab77e990df27dec196ea51ca394df5344f02cb0
Jerry Dong <rtdong9421@yahoo.com>
2020-06-26 14:43:27 -0400

    Updates

---
+++ precache-manifest.93c12aeac92701cb1f87ac14a5e04028.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a6c80398ad32afa16b094e60bb49bb2",
+    "url": "/project-six/index.html"
+  },
+  {
+    "revision": "b3007ef58879aa85085a",
+    "url": "/project-six/static/css/main.92545b3a.chunk.css"
+  },
+  {
+    "revision": "d8c11650e7a22175e6a3",
+    "url": "/project-six/static/js/2.dd470b07.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/project-six/static/js/2.dd470b07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3007ef58879aa85085a",
+    "url": "/project-six/static/js/main.cbd8b154.chunk.js"
+  },
+  {
+    "revision": "2135b200f8ca2a7f06b7",
+    "url": "/project-six/static/js/runtime-main.54c1688a.js"
+  }
+]);
