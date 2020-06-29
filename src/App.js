4f3ac1efbe6ed4984e4c4af1ba1d042cf7d368c2
smoshis <azevedoshonda@gmail.com>
2020-06-29 17:02:22 -0400

    added sass

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import './App.css';
+import './App.scss';
 import SearchBar from './SearchBar';
 import Slider from './Slider';
 import Gallery from './Gallery';
